1b7849ee274329a5bf8bca815d0d9aafe64616d0
Cody Swanson <Cody.S.Swanson@gmail.com>
2018-03-19 23:02:47 -0600

    Updated index page with link to functional website. Changed images in each town.

--- cit230/assignments/weathersite/scripts/wu-gweather.js
+++ cit230/assignments/weathersite/scripts/wu-gweather.js
@@ -1,12 +1,12 @@
-var weatherObjectfoo = new XMLHttpRequest();
+var weatherObjectG = new XMLHttpRequest();
 
-weatherObjectfoo.open('GET','https://api.wunderground.com/api/1adfc4042a009ff6/conditions/q/TX/Greenville.json', true );
+weatherObjectG.open('GET','https://api.wunderground.com/api/1adfc4042a009ff6/conditions/q/TX/Greenville.json', true );
 
-weatherObjectfoo.send();
+weatherObjectG.send();
 
-weatherObjectfoo.onload = function() {
+weatherObjectG.onload = function() {
     
-    var weatherInfo = JSON.parse(weatherObjectfoo.responseText);
+    var weatherInfo = JSON.parse(weatherObjectG.responseText);
     console.log(weatherInfo);
     
     document.getElementById('placeG').innerHTML = weatherInfo.current_observation.display_location.full;
@@ -24,15 +24,15 @@ weatherObjectfoo.onload = function() {
 
 
 
-var weatherForecastfoo = new XMLHttpRequest();
+var weatherForecastG = new XMLHttpRequest();
 
-weatherForecastfoo.open('GET','https://api.wunderground.com/api/1adfc4042a009ff6/forecast/q/TX/Greenville.json', true );
+weatherForecastG.open('GET','https://api.wunderground.com/api/1adfc4042a009ff6/forecast/q/TX/Greenville.json', true );
 
-weatherForecastfoo.send();
+weatherForecastG.send();
 
-weatherForecastfoo.onload = function() {
+weatherForecastG.onload = function() {
     
-    var neededInfo = JSON.parse(weatherForecastfoo.responseText);
+    var neededInfo = JSON.parse(weatherForecastG.responseText);
     console.log(neededInfo);
     
     document.getElementById('text_w_forecastG').innerHTML = neededInfo.forecast.txt_forecast.forecastday[0].fcttext;
